3361f96b4d1073e1ccfc5412fe458cd39dcfd9bb
SDozH <soldatov_alex@bk.ru>
2022-08-01 14:06:38 +0300

    Итог

---
+++ Unit10Final10.5/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unit10Final10._5
+{
+    public interface ILogger
+    {
+        void Error(string message);
+        void Event(string message);
+    }
+
+    class Calculator : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- Unit10Final10.5/Program.cs
+++ Unit10Final10.5/Program.cs
@@ -9,56 +9,35 @@ namespace Unit10Final10._5
         static void Main(string[] args)
         {
             Console.WriteLine("Вас приветствует I-Калькулятор");
+            ILogger logger = new Calculator();
+            Summator summator = new Summator(logger);
             while (true)
             {
-                ICalculator i = new Sumator();
-                Console.Write("Введите число  a: ");
-                int A;
-                while (!int.TryParse(Console.ReadLine(), out A))
-                    Console.Write("Вы ввели неверное значение \n" + "Введите a: ");
-
-                Console.Write("Введите число b: ");
-                int B;
-                while (!int.TryParse(Console.ReadLine(), out B))
-                    Console.Write("Вы ввели неверное значение \n" + "Введите b: ");
                 try
                 {
-                    var C = i.Sum(A, B);
-                    if (A == 0 && B == 0)
-                        throw new MyExeptions("Сложение нулей");
-                    if(A!=0 && B!=0)    
-                    Console.WriteLine("{0}+{1}={2}", A, B, C);
-                }
-                catch (MyExeptions e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-        }
+                    Console.Write("Введите число  a: ");
+                    int A;
+                    while (!int.TryParse(Console.ReadLine(), out A))
+                        Console.Write("Вы ввели неверное значение \n" + "Введите число a: ");
 
-        public interface ICalculator
-        {
-            int Sum(int a, int b);
-        }
-
-        public class Sumator : ICalculator
-        {
-            public int Sum(int a, int b)
-            {
-                int c = 0;
-                while (true)
-                {
-                    try
+                    Console.Write("Введите число b: ");
+                    int B;
+                    while (!int.TryParse(Console.ReadLine(), out B))
+                        Console.Write("Вы ввели неверное значение \n" + "Введите число b: ");
                     {
-                        c = a + b;
+                        if (A == 0 && B == 0)
+                            throw new MyExeptions("Сложение нулей бессмыслено");
                     }
-                    catch (MyExeptions e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                    return c;
+                    int result = summator.CheckAndSum(A, B);
+                    Console.WriteLine(result);
                 }
+                catch (MyExeptions ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
 
+                }
             }
 
         }
@@ -71,6 +50,29 @@ namespace Unit10Final10._5
             }
 
         }
+        internal class Summator : Calculator
+        {
+            ILogger logger;
+            Calculator calc;
+            public Summator(ILogger logger)
+            {
+                this.logger = logger;
+                calc = new Calculator();
+            }
+
+            public int CheckAndSum(int a, int b)
+            {
+
+                if (a == 0 || b == 0)
+                {
+                    logger.Error("Прибавлять ноль смысла не имеет");
+                    return calc.Sum(a, b);
+                }
+                logger.Event("Сумма чисел состовляет");
+                return calc.Sum(a, b);
+            }
+        }
     }
 }
+
 
